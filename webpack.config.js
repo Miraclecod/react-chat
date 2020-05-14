bac260a4624396cf33c130789dc8c706d1bdc976
Vitaly Pereslavtsev <dev@pereslavtsev.com>
2020-05-14 11:53:38 +0300

    initial

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const tsImportPluginFactory = require('ts-import-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+
+  output: {
+    filename: 'hotloader.js',
+    // the output bundle
+    path: resolve(__dirname, 'dist'),
+    publicPath: '/'
+    // necessary for HMR to know where to load the hot update chunks
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  devServer: {
+    port: '8080',
+    // Change it if other port needs to be used
+    hot: true,
+    // enable HMR on the server
+    noInfo: false,
+    quiet: false,
+    // minimize the output to terminal.
+    contentBase: resolve(__dirname, 'src'),
+    // match the output path
+    publicPath: '/',
+    // match the output `publicPath`
+    open: true
+  },
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader"
+          }
+        ]
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      },
+      { test: /\.png$/, loader: "url-loader?limit=100000" },
+      { test: /\.jpg$/, loader: "file-loader" },
+      { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' }
+    ]
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+      chunkFilename: "[id].css"
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    // enable HMR globally
+    new webpack.NamedModulesPlugin(),
+    // prints more readable module names in the browser console on HMR updates
+    new HtmlWebpackPlugin({template: resolve(__dirname, 'src/index.html')}),
+    // inject <script> in html file.
+  ],
+};
