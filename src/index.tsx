69ebd01589c43c53056fac8eefc7b9f8d26294c4
developer <ilyagneushev@noorsoft.ru>
2020-08-17 14:42:21 +0300

    Jira-DR5

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { Provider } from "react-redux";
+import  { createStore } from "redux";
 import { Hello } from "./components/Hello";
+
+
 
 ReactDOM.render(
   <Hello compiler="TypeScript" framework="React" />,
